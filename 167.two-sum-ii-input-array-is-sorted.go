a0608739f75e7f490d0e0ff577fc69efb3f4952b
Oskari Saari <okkuweb@protonmail.com>
2025-08-16 13:14:45 +0900

    167

---
+++ 167.two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,15 @@
+// @leet start
+func twoSum(numbers []int, target int) []int {
+	for x, v := range numbers {
+		for y, w := range numbers {
+			if v == w && x == y {
+				continue
+			}
+			if v + w == target {
+				return []int{x + 1, y + 1}
+			}
+		}
+	}
+	return nil
+}
+// @leet end
